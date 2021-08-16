a41994f552a1978d9b76ea66b9adfbdabf41b59a
Lucas Kovacs <lucas.kovacs@gmail.com>
2021-08-16 15:20:16 +0200

    + added easy admin

--- symfony/project/src/DataFixtures/AppFixtures.php
+++ symfony/project/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,24 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $blogPost = new BlogPost;
+        $blogPost->setTitle('A first post!');
+        $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
+        $blogPost->setContent('Post text!');
+        $blogPost->setAuthor('Lucas');
+        $blogPost->setSlug('a-first-post');
 
-        $manager->flush();
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost;
+        $blogPost->setTitle('A second post!');
+        $blogPost->setPublished(new \DateTime('2019-08-16 12:00:00'));
+        $blogPost->setContent('Post text second!');
+        $blogPost->setAuthor('Lucas');
+        $blogPost->setSlug('a-second-post');
+
+        $manager->persist($blogPost);
+
+        $manager->flush(); // execute / save
     }
 }
