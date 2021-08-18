275bd1e7b0c2c8ed1da430a5a9a2edb4b62c9900
Lucas Kovacs <lucas.kovacs@gmail.com>
2021-08-18 10:42:54 +0200

    - completed module 9

--- symfony/project/src/Entity/BlogPost.php
+++ symfony/project/src/Entity/BlogPost.php
@@ -3,15 +3,26 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\BlogPostRepository;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BlogPostRepository::class)
  * @ApiResource(
- *  itemOperations={"get"},
- *  collectionOperations={"get"}
+ *  itemOperations={
+ *      "get",
+ *      "put"={
+ *          "access_control"="is_granted('IS_AUTHENTICATED_FULLY') and object.getAuthor() == user"
+ *      }
+ *  },
+ *  collectionOperations={
+ *      "get",
+ *      "post"={
+ *          "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
+ *      }
+ *  }
  * )
  */
 class BlogPost
@@ -25,16 +36,22 @@ class BlogPost
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(min=10)
      */
     private $title;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\NotBlank()
+     * @Assert\DateTime()
      */
     private $published;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Length(min=20)
      */
     private $content;
 
@@ -46,6 +63,7 @@ class BlogPost
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank()
      */
     private $slug;
 
